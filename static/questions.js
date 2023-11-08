b1a7e662c2b7e50e5f4952ea942ea5b3eee481ef
AbsoluteReality <ambitious.abhiram001@gmail.com>
2023-11-08 20:55:32 +0530

    main

---
+++ static/questions.js
@@ -0,0 +1,240 @@
+questionsDict = {
+    "spanish": [
+        {
+            question: "How do you say 'hello' in Spanish?",
+            answers: [
+                {text: "Hola" ,correct: true},
+                {text: "Adios", correct: false},
+                {text: "Jabon", correct: false},
+                {text: "Hamos", correct: false}
+            ]
+        },
+        {
+            question: "How do you say 'dog' in Spanish?",
+            answers: [
+                {text: "Raton", correct: false},
+                {text: "Carro", correct: false},
+                {text: "Perro", correct: true},
+                {text: "Gato", correct: false}
+            ]
+        },
+        {
+            question: "How do you say 'Miss' in Spanish?",
+            answers: [
+                {text: "Cabeza", correct: false},
+                {text: "Senor", correct: false},
+                {text: "Senorito", correct: false},
+                {text: "Senorita", correct: true}
+            ]
+        },
+        {
+            question: "What is the correct phrase for Good Morning?",
+            answers: [
+                {text: "Medio Dia", correct: false},
+                {text: "Buenos Dias", correct: true},
+                {text: "Buenas Tardes", correct: false},
+                {text: "La Noche", correct: false}
+            ]
+        },
+        {
+            question: "Which one of these is “the man”?",
+            answers: [
+                {text: "El Hombre" ,correct: true},
+                {text: "La nina", correct: false},
+                {text: "El nino", correct: false},
+                {text: "La Mujer  ", correct: false}
+            ]
+        }
+    ],
+    "german": [
+        {
+            question: "How do you say “Please”?",
+            answers: [
+                {text: "Bitte", correct: true},
+                {text: "Danke", correct: false},
+                {text: "Tolle", correct: false},
+                {text: "Hamos", correct: false}
+            ]
+        },
+        {
+            question: "Wie viel kostet das? - Translate this phrase into English",
+            answers: [
+                {text: "How cold is it?", correct: false},
+                {text: "How far is that?", correct: false},
+                {text: "How much does that cost?", correct: true},
+                {text: "How are you?", correct: false}
+            ]
+        },
+        {
+            question: "How do you say 'Rain' in German?",
+            answers: [
+                {text: "Regnerisch ", correct: false},
+                {text: "Nahrung", correct: false},
+                {text: "Landabschnitt", correct: false},
+                {text: "Wasser", correct: true}
+            ]
+        },
+        {
+            question: "How do you say “Vegetable” in German?",
+            answers: [
+                {text: "Das Gemüse", correct: false},
+                {text: "Der Gemüse", correct: true},
+                {text: "Die Gemüse", correct: false},
+                {text: "Des Gemüse", correct: false}
+            ]
+        },
+        {
+            question: "How do you say “Black” in German?",
+            answers: [
+                {text: "Schwarz", correct: true},
+                {text: "Schwert", correct: false},
+                {text: "Schwart", correct: false},
+                {text: "Schmidt", correct: false}
+            ]
+        }
+    ],
+
+    "french": [
+        {
+            question: "How do you say “I eat”?",
+            answers: [
+                {text: "Je mange", correct: true},
+                {text: "Je manges", correct: false},
+                {text: "Je manger", correct: false},
+                {text: "Manager", correct: false}
+            ]
+        },
+        {
+            question: "How do you say “he visits” in French?",
+            answers: [
+                {text: "Ell visite", correct: false},
+                {text: "Ils visitent", correct: false},
+                {text: "Il visite", correct: true},
+                {text: "Je visite", correct: false}
+            ]
+        },
+        {
+            question: "How do you say 'Good Morning' in French?",
+            answers: [
+                {text: "Bonsoir " ,correct: false},
+                {text: "Bon après-midi", correct: false},
+                {text: "Bonjoir", correct: false},
+                {text: "Bonjour", correct: true}
+            ]
+        },
+        {
+            question: "How do you say “Water” in French?",
+            answers: [
+                {text: "Eau", correct: false},
+                {text: "Eae", correct: true},
+                {text: "Eat", correct: false},
+                {text: "Eaume", correct: false}
+            ]
+        },
+        {
+            question: "How do you say “White” in French?",
+            answers: [
+                {text: "Blanc" ,correct: true},
+                {text: "Blank", correct: false},
+                {text: "Black", correct: false},
+                {text: "Blase", correct: false}
+            ]
+        }
+    ],
+
+    english: [
+        {
+            question: "I came ___ America.",
+            answers: [
+                {text: "from" ,correct: true},
+                {text: "at", correct: false},
+                {text: "in", correct: false},
+                {text: "out", correct: false}
+            ]
+        },
+        {
+            question: "She is__ home.",
+            answers: [
+                {text: "in", correct: false},
+                {text: "on", correct: false},
+                {text: "from", correct: false},
+                {text: "at", correct: true}
+            ]
+        },
+        {
+            question: " She eats___ apple.",
+            answers: [
+                {text: "a " ,correct: false},
+                {text: "an", correct: false},
+                {text: "two", correct: false},
+                {text: "they", correct: true}
+            ]
+        },
+        {
+            question: "These __ books.",
+            answers: [
+                {text: "is", correct: false},
+                {text: "are", correct: true},
+                {text: "is a", correct: false},
+                {text: "am", correct: false}
+            ]
+        },
+        {
+            question: " How long____ learning English?",
+            answers: [
+                {text: "have you been" ,correct: true},
+                {text: "are you", correct: false},
+                {text: "did you", correct: false},
+                {text: "had you", correct: false}
+            ]
+        }
+    ],
+
+    "japanese": [
+        {
+            question: "How do you say “Good morning.”?",
+            answers: [
+                {text: "Ohayō" ,correct: true},
+                {text: "Ohayō gozaimasu", correct: false},
+                {text: "O-yasumi nasai", correct: false},
+                {text: "nasai", correct: false}
+            ]
+        },
+        {
+            question: "How do you say “30” in Japanese?",
+            answers: [
+                {text: "kyū", correct: false},
+                {text: "jū-yon", correct: false},
+                {text: "sanjū", correct: true},
+                {text: "nijū-ni", correct: false}
+            ]
+        },
+        {
+            question: "How do you say 'Hello' in Japanese?",
+            answers: [
+                {text: "Bonsoir " ,correct: false},
+                {text: "nara", correct: false},
+                {text: "sayonara", correct: false},
+                {text: "konnichiwa", correct: true}
+            ]
+        },
+        {
+            question: "How do you say “Land” in Japanese?",
+            answers: [
+                {text: "tochi", correct: false},
+                {text: "tochiua", correct: true},
+                {text: "tochima", correct: false},
+                {text: "tochika", correct: false}
+            ]
+        },
+        {
+            question: "How do you say “Grey” in Japanese?",
+            answers: [
+                {text: "haiiro" ,correct: true},
+                {text: "ao", correct: false},
+                {text: "kiiro", correct: false},
+                {text: "shiro", correct: false}
+            ]
+        }
+    ]
+};
